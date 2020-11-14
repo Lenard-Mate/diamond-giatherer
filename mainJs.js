82a6311e7fa151ae0ca2bde871759a90c18d768b
Leni <lenard.mate.98@gmail.com>
2020-11-14 15:53:56 +0200

    Tema_1

---
+++ mainJs.js
@@ -0,0 +1,31 @@
+
+
+var person = {
+
+  firstName:"Barle",
+  lastName:"Bendrea",
+  eyeColor:"albastru",
+  age:21,
+
+  printPerson:function(){
+  	console.log(person);
+  	document.getElementById("personData").innerHTML=""+person.firstName+","+person.lastName+","+person.eyeColor+","+person.age;
+  }
+
+};
+
+person.printPerson();
+
+function writeText(){
+
+	var value=document.getElementById("name").value;
+	console.log(value);
+	document.getElementById("valueOfInput").innerHTML=nameReturning(value);
+}
+
+function nameReturning(personName){
+
+	console.log("Buna,numele meu este "+personName);
+
+return "Buna,numele meu este "+personName;
+}
